772c2a6050396ef60b94f0a51c9bc79570af1b0c
Beth Carey <bethcarey@thinkingsolutions.com.au>
2016-01-03 12:55:37 +1100

    Updated NuGet packages. Added versioning tracking for web app based on a new class.

---
+++ TSTuring2015.MVC5Web/Models/Versioning.cs
@@ -0,0 +1,12 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Versioning.cs" company="Thinking Solutions Pty Ltd">
+//     Copyright (c) Thinking Solutions 2015. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace TSTuring2015.MVCWeb.Models
+{
+    public class Versioning
+    {
+        public static string Current { get; } = "1.0-0001j";
+    }
+}
